e5c27a3aca2f70f7459a107663e2090e6b1b0a4b
Jessica <jesbromac@alu.edu.gva.es>
2025-11-17 22:46:05 +0100

    backgroud puesto

--- app/src/main/java/cartasdroprating/EstadoCartas.kt
+++ app/src/main/java/cartasdroprating/EstadoCartas.kt
@@ -1,6 +1,8 @@
 package net.iessochoa.jessicabrotons.cartasfer
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -19,6 +21,7 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -66,6 +69,8 @@ fun MostrarEjercicio(){
 
         Column(
             modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xFFE3DAC9))
                 .padding(innerPadding)
                 .padding(16.dp)
         ) {
